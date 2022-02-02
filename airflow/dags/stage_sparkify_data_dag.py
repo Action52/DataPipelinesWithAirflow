6f701a21460c33b5beaed6f44133796df920a559
Luis Alfredo León Villapún <alfredo.leon@verato.com>
2022-02-02 10:00:37 -0600

    Modified script to suit required project folder structure.

--- airflow/dags/stage_sparkify_data_dag.py
+++ airflow/dags/stage_sparkify_data_dag.py
@@ -1,4 +1,4 @@
-from airflow import DAG
+from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
 from operators import (RedshiftStagingOperator, LoadFactOperator, LoadDimensionOperator, DataQualityOperator)
